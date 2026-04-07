8068f60fbb7f6f2a9d15a168154cce19c26b00b1
Manikandan2005ms <mmani786ms@gmail.com>
2026-04-07 10:33:45 +0530

    Initial commit

---
+++ app/src/main/java/com/varshu/energyauditapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.varshu.energyauditapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+        supportActionBar?.hide()
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            cacheMode = WebSettings.LOAD_NO_CACHE
+            allowFileAccess = true
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/EnergyAuditApp.html")
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        })
+    }
+}
